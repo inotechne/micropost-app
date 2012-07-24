39282aacce394d19f5c28629f9f6e74f44d9291c
Pratompol Jiravanitchakorn <boat.pratompol@gmail.com>
2012-07-24 12:29:35 +0700

    Create new rails application named micropost-app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicropostApp::Application.config.secret_token = '7ee258bd415506911b1585677810aa2ab2e6999948fb08eee279a10dfa3790f0a86a105473bcc5db28a48c213d1c668b582f33208c92c206ef60ca531e445577'
