1cca7cb8d3d57f72967fef565358a96374d406cd
monthas <monsiree.t@gmail.com>
2012-08-06 12:49:57 +0700

    create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Create Users ..."
+
+user1 = User.create(:name => "John Smith", :email => "john@inotechne.com",
+                    :password => "johntest", :password_confirmation => "johntest")
+puts "User1 [name: #{user1.name}]  [email: #{user1.email}]  [password: #{user1.password}]"
+
+user2 = User.create(:name => "Anny Smith", :email => "anny@inotechne.com",
+                    :password => "annytest", :password_confirmation=> "annytest")
+puts "User2 [name: #{user2.name}]  [email: #{user2.email}]  [password: #{user2.password}]"
+puts "================================================================================"
+
+puts "Create Relationship ..."
+Relationship.create(:follower => user2, :following => user1)
+puts "#{user2.name} follow #{user1.name}"
+
+puts "================================================================================"
+
